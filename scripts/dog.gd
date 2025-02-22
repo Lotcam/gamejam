99f54c5d48ecdfbdc9c424627d85f7e282685c6f
Andres Lot Camarena <lotcamandres@gmail.com>
2025-02-22 22:59:01 +0100

    basic dog

---
+++ scripts/dog.gd
@@ -0,0 +1,14 @@
+extends CharacterBody3D
+
+func feed():
+	print("fed doggo")
+	#TODO: score up
+	queue_free()
+
+func _on_timer_feed_despawn_timeout() -> void:
+	queue_free()
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("bullet"):
+		feed()
