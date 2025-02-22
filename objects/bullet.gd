cdaf3a0e547b81293ccbaa91b1f06d42896c1137
Andres Lot Camarena <lotcamandres@gmail.com>
2025-02-23 00:19:53 +0100

    dog + bullet despawn

--- objects/bullet.gd
+++ objects/bullet.gd
@@ -2,4 +2,7 @@ extends RigidBody3D
 
 
 func _on_life_time_timeout():
+	despawn()
+
+func despawn():
 	queue_free()
--- scripts/dog.gd
+++ scripts/dog.gd
@@ -12,3 +12,4 @@ func _on_timer_feed_despawn_timeout() -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("bullet"):
 		feed()
+		if body.has_method("despawn"): body.despawn()
