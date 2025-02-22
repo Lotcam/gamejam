52777518118a905e7bbdabf8de4715367b807422
Andres Lot Camarena <lotcamandres@gmail.com>
2025-02-22 23:55:55 +0100

    hotfix gun

---
+++ gun.gd
@@ -0,0 +1,26 @@
+extends XRToolsPickable
+
+@export var bullet : PackedScene
+@export var bullet_speed : float = 10.0
+var can_fire = true
+
+func action():
+	super.action()
+	
+	if can_fire:
+		_spawn_bullets()
+		can_fire = false
+		$Cooldown.start()
+
+func _spawn_bullets():
+	if bullet:
+		var new_bullet : RigidBody3D = bullet.instantiate()
+		if new_bullet:
+			var spawn_global_transform = $SpawnPoint.global_transform
+			add_child(new_bullet)
+			new_bullet.global_transform = spawn_global_transform
+			new_bullet.reparent(get_tree().current_scene, true)
+			new_bullet.linear_velocity = spawn_global_transform.basis.x * bullet_speed
+
+func _on_cooldown_timeout():
+	can_fire = true
