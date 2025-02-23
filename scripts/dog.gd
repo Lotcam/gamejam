8b62a1d4f92f7c64a5b07a3d604a3e6c29715f58
Dragosh <johnnytanner231197@gmail.com>
2025-02-23 11:57:06 +0100

    less movement

--- scripts/dog.gd
+++ scripts/dog.gd
@@ -10,16 +10,16 @@ var moving = true
 @onready var movement = $Movement
 
 func _physics_process(_delta):
-    if moving:
-        move_and_slide()
-    
+	if moving:
+		move_and_slide()
+	
 func feed(meat: Util.MEAT_STATE):
 	Global.score_up(meat)
 	print("score: " + str(Global.score))
 	queue_free()
 
 func _on_timer_feed_despawn_timeout() -> void:
-    queue_free()
+	queue_free()
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
@@ -28,19 +28,19 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		if body.has_method("despawn"): body.despawn()
 
 func initialize(start_position, pivot_position):
-    look_at_from_position(start_position, pivot_position, Vector3.UP)
-    rotate_y(randf_range(-PI / 4, PI / 4))
-    
-    # Movement
-    var random_speed = randi_range(min_speed, max_speed)
-    velocity = Vector3.FORWARD * random_speed
-    velocity = velocity.rotated(Vector3.UP, rotation.y)
+	look_at_from_position(start_position, pivot_position, Vector3.UP)
+	rotate_y(randf_range(-PI / 4, PI / 4))
+	
+	# Movement
+	var random_speed = randi_range(min_speed, max_speed)
+	velocity = Vector3.FORWARD * random_speed
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
 
 func stop_movement():
-    moving = false
-    velocity = Vector3.ZERO
+	moving = false
+	velocity = Vector3.ZERO
 
 
 func _on_visible_on_screen_notifier_3d_screen_exited():
-    queue_free()
+	queue_free()
