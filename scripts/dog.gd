ba994bd40976071d336634df6bd914f0ffeaff8e
Andres Lot Camarena <lotcamandres@gmail.com>
2025-02-23 12:21:58 +0100

    score show

--- scripts/dog.gd
+++ scripts/dog.gd
@@ -1,8 +1,17 @@
 extends CharacterBody3D
 
+@export var score: PackedScene
+
 func feed(meat: Util.MEAT_STATE):
 	Global.score_up(meat)
 	print("score: " + str(Global.score))
+	var new_score = score.instantiate()
+	new_score.initialize(meat)
+	if new_score:
+		var spawn_global_transform = $spawn_score.global_transform
+		add_child(new_score)
+		new_score.global_transform = spawn_global_transform
+		new_score.reparent(get_tree().current_scene, true)
 	queue_free()
 
 func _on_timer_feed_despawn_timeout() -> void:
