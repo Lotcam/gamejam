f515dd82a007302fa1e31327a2308671b1eb8b66
Dragosh <johnnytanner231197@gmail.com>
2025-02-23 11:43:44 +0100

    spawner dogs stop

--- levels/main.gd
+++ levels/main.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 var xr_interface: XRInterface
 
+@export var dog_scene : PackedScene
+
 func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
 	if xr_interface and xr_interface.is_initialized():
@@ -14,3 +16,14 @@ func _ready():
 		get_viewport().use_xr = true
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
+	
+
+func _on_dog_timer_timeout():
+	var dog = dog_scene.instantiate()
+	var dog_spawn_location = get_node("SpawnPath/SpawnLocation")
+	dog_spawn_location.progress_ratio = randf()
+
+	var pivot_position = $Pivot.position
+	dog.initialize(dog_spawn_location.position, pivot_position)
+
+	add_child(dog)
--- scripts/dog.gd
+++ scripts/dog.gd
@@ -1,15 +1,46 @@
 extends CharacterBody3D
 
+# Minimum speed of the mob in meters per second.
+@export var min_speed = 5
+# Maximum speed of the mob in meters per second.
+@export var max_speed = 6
+
+var moving = true
+
+@onready var movement = $Movement
+
+func _physics_process(_delta):
+    if moving:
+        move_and_slide()
+    
 func feed():
-	print("fed doggo")
-	#TODO: score up
-	queue_free()
+    print("fed doggo")
+    #TODO: score up
+    queue_free()
 
 func _on_timer_feed_despawn_timeout() -> void:
-	queue_free()
+    queue_free()
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if body.is_in_group("bullet"):
-		feed()
-		if body.has_method("despawn"): body.despawn()
+    if body.is_in_group("bullet"):
+        feed()
+        if body.has_method("despawn"): body.despawn()
+
+func initialize(start_position, pivot_position):
+    look_at_from_position(start_position, pivot_position, Vector3.UP)
+    rotate_y(randf_range(-PI / 4, PI / 4))
+    
+    # Movement
+    var random_speed = randi_range(min_speed, max_speed)
+    velocity = Vector3.FORWARD * random_speed
+    velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+
+func stop_movement():
+    moving = false
+    velocity = Vector3.ZERO
+
+
+func _on_visible_on_screen_notifier_3d_screen_exited():
+    queue_free()
